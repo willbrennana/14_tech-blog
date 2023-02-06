71f3b8e1037f3422d67ebfa4c1332436f1e32390
willbrennana <will.brennana@gmail.com>
2023-02-06 12:28:31 -0600

    Added get route for single post to leave a comment in home-routes.js file.

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -21,4 +21,28 @@ router.get("/login", (req, res) => {
   res.render("login");
 });
 
+router.get("/post/:id", async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        User,
+        {
+          model: Comment,
+          include: [User],
+        },
+      ],
+    });
+    if (postData) {
+      const post = postData.get({
+        plain: true,
+      });
+      res.render("single-post", { post });
+    } else {
+      res.status(404).end();
+    }
+  } catch (err) {
+    res.status(500).end;
+  }
+});
+
 module.exports = router;
