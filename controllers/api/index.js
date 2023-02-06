aeb0c1b37095e9df32b6b4fe98c5adbba319377e
willbrennana <will.brennana@gmail.com>
2023-02-06 12:45:36 -0600

    Updated index.js in api folder to include comment routes and index.js in larger controllers folder to include dashboard routes.

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -8,4 +8,8 @@ const userRoutes = require("./user-routes");
 
 router.use("/users", userRoutes);
 
+const commentRoutes = require("./comment-routes");
+
+router.use("/comment", commentRoutes);
+
 module.exports = router;
