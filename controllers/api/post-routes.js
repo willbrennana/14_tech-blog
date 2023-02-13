54cb1696cff25a5479589a5ba96557f791fb24ab
willbrennana <will.brennana@gmail.com>
2023-02-13 12:02:22 -0600

    Added dashboard functionality to js and handlebars folders, also updated user_id under postData const in post-routes.js and created connection to dbin connection.js

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -6,7 +6,7 @@ router.post("/", async (req, res) => {
     const postData = await Post.create({
       title: req.body.title,
       body: req.body.body,
-      user_id: req.body.user_id,
+      user_id: req.session.userId,
     });
     res.status(200).json(postData);
   } catch (err) {
---
+++ public/js/dashboard.js
@@ -0,0 +1,26 @@
+const newFormHandler = async (event) => {
+  event.preventDefault();
+
+  const title = document.querySelector("#post-title").value.trim();
+  const body = document.querySelector("#post-body").value.trim();
+
+  if (title && body) {
+    const response = await fetch(`/api/post`, {
+      method: "POST",
+      body: JSON.stringify({ title, body, user_id }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (response.ok) {
+      document.location.replace("/profile");
+    } else {
+      alert("Failed to create project");
+    }
+  }
+};
+
+document
+  .querySelector(".new-post-form")
+  .addEventListener("submit", newFormHandler);
